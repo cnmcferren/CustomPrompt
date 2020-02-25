1fb380939c86821e09f48e877195cbf8ff5c347a
Conor McFerren <cnmcferren@gmail.com>
2020-02-25 12:50:38 -0500

    Moved colors definitions

---
+++ src/colors.h
@@ -0,0 +1,23 @@
+#ifndef __COLORS_H_INCLUDE__
+#define __COLORS_H_INCLUDE__
+
+//Colors
+#define ARROW ""
+#define BRANCH ""
+#define DGRAYORANGE "\033[48;5;202;38;5;243m"
+#define ORANGEDGRAY "\033[38;5;202;48;5;243m"
+#define DGRAYDEFAULT "\033[38;5;243;48;5;234m"
+#define WHITEDGRAY "\033[38;5;255;48;5;243m"
+#define WHITEORANGE "\033[38;5;255;48;5;202m"
+#define FBBW "\033[38;5;232;48;5;15m"
+#define FWBR "\033[38;5;15;48;5;196m"
+#define FRBW "\033[38;5;196;48;5;15m"
+#define FWBD "\033[38;5;15;48;5;234m"
+#define FGBB "\033[38;5;46;48;5;33m"
+#define FBBG "\033[38;5;33;48;5;46m"
+#define FGBB "\033[38;5;46;48;5;33m"
+#define FBBD "\033[38;5;33;48;5;234m"
+#define RESET "\033[0m"
+#define BLINK "\033[5m"
+
+#endif
